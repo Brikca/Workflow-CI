f89e26d6f435a737bd025d54fdd5249c24be6faa
Brikca Kristal <kdesfingka@gmail.com>
2026-06-09 09:43:00 +0700

    Create modelling.py

---
+++ MLProject/modelling.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+import mlflow
+import mlflow.sklearn
+
+# =========================
+# Load Dataset
+# =========================
+df = pd.read_csv("telco_preprocessed.csv")
+
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
+
+# =========================
+# Split Data
+# =========================
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+# =========================
+# MLflow
+# =========================
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+mlflow.set_experiment("Telco-Churn-Basic")
+
+mlflow.sklearn.autolog()
+
+# =========================
+# Training
+# =========================
+with mlflow.start_run(run_name="RandomForest-Autolog"):
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        class_weight="balanced",
+        random_state=42,
+        n_jobs=-1
+    )
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    print("Training selesai")
+    print(f"Accuracy: {acc:.4f}")
